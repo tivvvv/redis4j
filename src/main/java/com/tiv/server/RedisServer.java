ab5089f7dbdea46495994dad3bd67a04c576126e
tivvvv <18612553935@163.com>
2025-05-14 08:16:19 +0800

    feat: 服务端初始化.

---
+++ src/main/java/com/tiv/server/RedisServer.java
@@ -0,0 +1,8 @@
+package com.tiv.server;
+
+public interface RedisServer {
+
+    void start();
+
+    void stop();
+}
---
+++ src/main/java/com/tiv/server/impl/RedisServerImpl.java
@@ -0,0 +1,71 @@
+package com.tiv.server.impl;
+
+import com.tiv.server.RedisServer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RedisServerImpl implements RedisServer {
+
+    private String host;
+
+    private int port;
+
+    private EventLoopGroup bossGroup;
+
+    private EventLoopGroup workerGroup;
+
+    private Channel serverChannel;
+
+    public RedisServerImpl(String host, int port) {
+        this.host = host;
+        this.port = port;
+        this.bossGroup = new NioEventLoopGroup(1);
+        this.workerGroup = new NioEventLoopGroup(5);
+    }
+
+    @Override
+    public void start() {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new StringDecoder());
+                        pipeline.addLast(new StringEncoder());
+                    }
+                });
+        try {
+            serverChannel = serverBootstrap.bind(host, port).sync().channel();
+            log.info("Redis Server started at host: {} port: {}", host, port);
+        } catch (InterruptedException e) {
+            log.error("Redis Server start failed", e);
+            stop();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (bossGroup != null) {
+            bossGroup.shutdownGracefully();
+        }
+        if (workerGroup != null) {
+            workerGroup.shutdownGracefully();
+        }
+        if (serverChannel != null) {
+            serverChannel.close();
+        }
+    }
+}
